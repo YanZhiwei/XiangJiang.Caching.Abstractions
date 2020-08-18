013230aa901db1834ccbef7dd8d09bc7349796b5
YanZhiwei <Yan.Zhiwei@hotmail.com>
2020-08-18 23:54:18 +0800

    add contract

--- XiangJiang.Caching.Abstractions/ICacheProvider.cs
+++ XiangJiang.Caching.Abstractions/ICacheProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace XiangJiang.Caching.Abstractions
+﻿using System.Threading.Tasks;
+
+namespace XiangJiang.Caching.Abstractions
 {
     /// <summary>
     ///     缓存提供者接口
@@ -13,6 +15,8 @@
         /// <returns>缓存</returns>
         T Get<T>(string key);
 
+        Task<T> GetAsync<T>(string key);
+
         /// <summary>
         ///     设置缓存
         /// </summary>
@@ -20,6 +24,8 @@
         /// <param name="data">值</param>
         /// <param name="cacheTimeMinute">过期时间，单位分钟</param>
         void Set(string key, object data, uint cacheTimeMinute);
+
+        Task SetAsync(string key, object data, uint cacheTimeMinute);
 
         /// <summary>
         ///     是否设置缓存
@@ -30,16 +36,22 @@
         /// </returns>
         bool IsSet(string key);
 
+        Task<bool> IsSetAsync(string key);
+
         /// <summary>
         ///     移除缓存
         /// </summary>
         /// <param name="key">键</param>
         void Remove(string key);
 
+        Task RemoveAsync(string key);
+
         /// <summary>
         ///     根据正则表达式移除缓存
         /// </summary>
         /// <param name="pattern">移除缓存</param>
         void RemoveByPattern(string pattern);
+
+        Task RemoveByPatternAsync(string pattern);
     }
 }
